97443e2bf7eeaadf8388a3d4ac26cd5bd341ccbf
Zack Morelli <zackmorelli@gmail.com>
2020-12-16 15:41:22 -0500

    Add files via upload

---
+++ Normal Tissue Complication Probability Calculator/GUI.cs
@@ -0,0 +1,426 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VMS.TPS;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+
+
+
+
+
+/*
+    Liver Only Normal Tissue Complication Probability (NTCP) Calculator - ESAPI 15.6 version (3/16/2020).
+
+    This is the source code for the Windows Forms GUI used in the Liver Only Normal Tissue Complication Probability (NTCP) Calculator program. It is kept as a separate file from the main file.
+
+    This program is expressely written as a plug-in script for use with Varian's Eclipse Treatment Planning System, and requires Varian's API files to run properly.
+    This program also requires .NET Framework 4.5.0 to run properly, and the MathNet.Numerics class library package, which is freely availible on the NuGet Package manager in Visual Studio.
+    The MathNet.Numerics package contains an Error function method which is used in the NTCP calculation.
+
+    Copyright (C) 2020 Zackary Thomas Ricci Morelli
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+    I can be contacted at: zackmorelli@gmail.com
+                           zackary.t.morelli@lahey.org
+
+
+*/
+
+
+
+namespace NTCPcalc
+{
+    public partial class GUI : Form
+    {
+        public string pl = null;
+        public string org = null;
+        double TD50 = 0.0;
+      //  double ab = 0.0;
+        string ty;
+        // double n = 0.0;
+        // double m = 0.0;
+        List<string> plannames = new List<string>();
+        List<string> sumnames = new List<string>();
+
+        public GUI(IEnumerable<PlanSum> Plansums, IEnumerable<PlanSetup> Plans)
+        {
+            // MessageBox.Show("Trig 5");
+            InitializeComponent();
+ 
+            // MessageBox.Show("Trig 6");
+
+            foreach (PlanSetup aplan in Plans)
+            {
+                PlanList.Items.Add(aplan.Id);
+                plannames.Add(aplan.Id);
+                // MessageBox.Show("Trig 8");
+            }
+
+            ExecuteCalc.Click += (sender, EventArgs) => { buttonNext_Click(sender, EventArgs, Plansums, Plans); };
+
+   
+            //  MessageBox.Show("Trig GUI END");
+        }  // end of actual GUI execution
+
+        //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        //associated functions
+
+        private double Veff(DVHData sDVH, double RX, double Vol)
+        {
+            double VE = 0.0;
+            double TotVol = 0.0;
+
+            //  MessageBox.Show("sDVH.CurveData[50].DoseValue is: " + sDVH.CurveData[50].DoseValue);
+
+            foreach (DVHPoint point in sDVH.CurveData)
+            {
+                TotVol += point.Volume;
+
+                if (point.Volume < 0.00025)    // ends volume sum before very end to prevent sum of strange values at very end
+                {
+                    break;
+                }
+            }
+
+           //  MessageBox.Show("Totvol is: " + TotVol);
+
+            double Area = (TotVol * 0.1);  // Volume Sum multiplied by the Dose step size gives the area under the DVH curve for the organ in question
+
+            double AbsVeff = (Area / (RX * 100.0));  // this gives the Veff in cc
+
+            // MessageBox.Show("ABSVeff is: " + AbsVeff);
+           //  MessageBox.Show("Vol is: " + Vol);
+
+            VE = (AbsVeff / Vol);  // divide the Veff by the total volume of the organ to express Veff as a percentage
+
+            //  MessageBox.Show("VE is: " + VE);
+
+            return VE;
+        }
+
+        private double NTCPCALC(double VE, double ab, double Dmax, int fracs, double n, double m)
+        {
+             // MessageBox.Show("Dmax is: " + Dmax.ToString());
+            //  double Fracs = (double)fracs;
+           //   MessageBox.Show("Fracs is: " + fracs.ToString());
+          // MessageBox.Show("VE is: " + VE.ToString());
+          //  MessageBox.Show("TD50 is: " + TD50.ToString());
+           // MessageBox.Show("ab is: " + ab.ToString());
+          //  MessageBox.Show("Rx is: " + RX.ToString());
+          //  MessageBox.Show("Vol is: " + Vol.ToString());
+         //  MessageBox.Show("n is: " + n.ToString());
+         //   MessageBox.Show("m is: " + m.ToString());
+
+            if(TD50 == 0)
+            {
+                MessageBox.Show("Please select either METS or HCC for the calculation to work properly");
+            }
+
+            double NTCP = 0.0;
+
+
+            double NTD2 = Dmax * (((Dmax / fracs) + ab) / (2 + ab));
+
+            double TDVeff = TD50 * (Math.Pow(VE, -n));
+
+            double t = ((NTD2 - TDVeff) / TDVeff) / m;  //  t is the parameter given to the ERF function
+
+            // NTCP = 1/2(ERF(t/sqrt(2)) + 1)
+
+            double erf = MathNet.Numerics.SpecialFunctions.Erf((t / Math.Sqrt(2)));
+
+            NTCP = (erf + 1) * 0.5;
+
+             // MessageBox.Show("NTCP is: " + NTCP);    used to show the whole value and verify calculation
+            return NTCP;
+        }
+
+        private void EXECUTE(IEnumerable<PlanSum> Plansums, IEnumerable<PlanSetup> Plans)
+        {
+            bool livlock = false;
+            double n = 1.0;         // Volume effect parameter of the Lyman-Kutcher-Burman model. Set here to one in accordance with the Lahey SBRT Liver Protocol
+            double m = 0.12;        // Slope parameter of the Lyman-Kutcher-Burman model. set here to 0.12 in accordance with the Lahey SBRT Liver Protocol
+            double ab = 3.0;        // alpha/beta radation sensitivity ratio of Livers. Originally comes from Linear-Quadratic model, used in LKB as well.
+            double RX = 0.0;        //prescribed dose of the organ in cGy
+            double Dmax = 0.0;     // max dose of the organ
+            double Vol = 0.0;      // volume of the organ
+            int fracs = 0;          // number of fractions to the organ
+
+           // MessageBox.Show("Trig EXE - 1");
+
+            IEnumerator ER = Plans.GetEnumerator();
+            ER.MoveNext();
+            PlanSetup Plan = (PlanSetup)ER.Current;
+
+            if (Plan.Id == pl)
+            {
+                // MessageBox.Show("Trig EXE - 2");
+                Plan = (PlanSetup)ER.Current;
+            }
+            else
+            {
+                ER.MoveNext();
+                Plan = (PlanSetup)ER.Current;
+                if (Plan.Id == pl)
+                {
+                    Plan = (PlanSetup)ER.Current;
+                   // MessageBox.Show("Trig EXE - 3");
+                }
+                else
+                {
+                    ER.MoveNext();
+                    Plan = (PlanSetup)ER.Current;
+                    if (Plan.Id == pl)
+                    {
+                        Plan = (PlanSetup)ER.Current;
+                    }
+                    else
+                    {
+                        ER.MoveNext();
+                        Plan = (PlanSetup)ER.Current;
+                        if (Plan.Id == pl)
+                        {
+                            Plan = (PlanSetup)ER.Current;
+                        }
+                        else
+                        {
+                            ER.MoveNext();
+                            Plan = (PlanSetup)ER.Current;
+                            if (Plan.Id == pl)
+                            {
+                                Plan = (PlanSetup)ER.Current;
+                            }
+                            else
+                            {
+                                ER.MoveNext();
+                                Plan = (PlanSetup)ER.Current;
+                                if (Plan.Id == pl)
+                                {
+                                    Plan = (PlanSetup)ER.Current;
+                                }
+                                else
+                                {
+                                    ER.MoveNext();
+                                    Plan = (PlanSetup)ER.Current;
+                                    if (Plan.Id == pl)
+                                    {
+                                        Plan = (PlanSetup)ER.Current;
+                                    }
+                                    else
+                                    {
+                                        ER.MoveNext();
+                                        Plan = (PlanSetup)ER.Current;
+                                        if (Plan.Id == pl)
+                                        {
+                                            Plan = (PlanSetup)ER.Current;
+                                        }
+                                        else
+                                        {
+                                            ER.MoveNext();
+                                            Plan = (PlanSetup)ER.Current;
+                                            if (Plan.Id == pl)
+                                            {
+                                                Plan = (PlanSetup)ER.Current;
+                                            }
+                                            else
+                                            {
+                                                ER.MoveNext();
+                                                Plan = (PlanSetup)ER.Current;
+                                                if (Plan.Id == pl)
+                                                {
+                                                    Plan = (PlanSetup)ER.Current;
+                                                }
+                                                else
+                                                {
+                                                    ER.MoveNext();
+                                                    Plan = (PlanSetup)ER.Current;
+                                                    if (Plan.Id == pl)
+                                                    {
+                                                        Plan = (PlanSetup)ER.Current;
+                                                    }
+                                                    else
+                                                    {
+                                                        MessageBox.Show("Could not find the selected plan!");
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+          //  MessageBox.Show("Trig EXE - 10");
+
+          RX = Plan.TotalDose.Dose;
+
+          try
+          {
+               fracs = (int)Plan.NumberOfFractions;
+          }
+          catch
+          {
+                MessageBox.Show("An error ocurred when attempting to retrieve the Number of Fractions of this plan. A default value of 5 will be used.");
+                fracs = 5;
+          }
+                
+          IEnumerator ZK = Plan.StructureSet.Structures.GetEnumerator();      // all of this stuff from line 157 to 168 is just so we can get an initialized structure variable that we can use to instaiatiate a DVHdata variable, all because we don't have write access to Varian's API
+                                                                                    //  MessageBox.Show("Trig EXE - f3.1");
+          ZK.MoveNext();
+          // MessageBox.Show("Trig EXE - 11");
+
+          Structure STR = (Structure)ZK.Current;
+         // MessageBox.Show("Trig EXE - f3.3");
+
+         
+
+          foreach (Structure S in Plan.StructureSet.Structures)
+          {
+              if (S.Id == "Liver")
+              {
+                  //  MessageBox.Show("S.Id is: " + S.Id.ToString());
+                  STR = S;
+                  livlock = true;
+                  // MessageBox.Show("Trig EXE - f3.4");
+              }
+          }
+
+          if(livlock == false)
+          {
+                MessageBox.Show("Warning: No liver structure was found in this plan! Unable to perform calculation.");
+                return;
+          }
+
+          DVHData sDVH = Plan.GetDVHCumulativeData(STR, DoseValuePresentation.Absolute, VolumePresentation.AbsoluteCm3, 0.1);
+
+          // MessageBox.Show("DVH fetched successfully!");
+
+          Vol = STR.Volume;
+          Dmax = 0.0;
+
+            //  DoseValue maxdose = kDVH.MaxDose;
+
+            foreach (DVHPoint point in sDVH.CurveData)
+            {
+                if (point.Volume < 0.1 && point.Volume > 0.03)
+                {
+                    // Console.WriteLine("\n  DVH Point VOLUME: {0}", point.Volume);
+
+                    if (Dmax == 0.0)
+                    {
+                        Dmax = point.DoseValue.Dose;
+                    }
+
+                    if (point.DoseValue.Dose > Dmax)
+                    {
+                        Dmax = point.DoseValue.Dose;
+                    }
+                }
+            }
+
+          RX = (RX / 100.0);       // converting cGy to Gy
+          Dmax = (Dmax / 100.0);
+
+          double VE = Veff(sDVH, RX, Vol);
+
+          VeffOut.Text = Math.Round((VE * 100.0), 1, MidpointRounding.AwayFromZero) + "%";
+
+          double NTCP = NTCPCALC(VE, ab, Dmax, fracs, n, m);
+
+          NTCPout.Text = Math.Round((NTCP * 100.0), 1, MidpointRounding.AwayFromZero) + "%";
+        }
+
+        private void ExecuteCalc_Click(object sender, EventArgs args)
+        {
+           
+            //  MessageBox.Show("Organ: " + org.ToString());
+            //  MessageBox.Show("Trig 12 - First Click");
+        }
+
+        void buttonNext_Click(object sender, EventArgs e, IEnumerable<PlanSum> PLS, IEnumerable<PlanSetup> PLN)
+        {
+            // MessageBox.Show("Trig MORTY");
+            EXECUTE(PLS, PLN);
+        }
+
+        void PlanList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pl = PlanList.SelectedItem.ToString();
+
+            if (pl == "Motion Assess" || pl == "motion assess" || pl == "Mot Assess" || pl == "mot assess")
+            {
+                MessageBox.Show("This script is not compatible with Motion Assess plans!");
+                return;
+            }
+            //  MessageBox.Show("Trig 10");
+           
+        }
+        
+        void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // MessageBox.Show("checkedlistboxfire");
+            if (listBox1.SelectedItem.ToString() == "HCC")
+            {
+                ty = "HCC";
+                TD50 = 39.8;
+            }
+            else if (listBox1.SelectedItem.ToString() == "METS")
+            {
+                ty = "METS";
+                TD50 = 45.8;
+            }
+        }
+
+        private void Directions_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NTCPout_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /*
+        void checkedListBox1_ItemCheck(object sender, EventArgs e)
+        {
+            if (checkedListBox1.GetItemChecked(0))
+            {
+                ty = "HCC";
+                TD50 = 39.8;
+            }
+            else if (checkedListBox1.GetItemChecked(1))
+            {
+                ty = "METS";
+                TD50 = 45.8;
+            }
+        }
+        */
+    }
+}
---
+++ Normal Tissue Complication Probability Calculator/NTCPcalc.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+
+
+/*
+    Liver Only Normal Tissue Complication Probability (NTCP) Calculator - ESAPI 15.6 version (3/16/2020)
+    This program is expressely written as a plug-in script for use with Varian's Eclipse Treatment Planning System, and requires Varian's API files to run properly.
+    This program also requires .NET Framework 4.5.0 to run properly, and the MathNet.Numerics class library package, which is freely availible on the NuGet Package manager in Visual Studio, However I have also included a copy here.
+    The MathNet.Numerics package contains an Error function method which is used in the NTCP calculation.
+    This C# source code file is technically for a class library, or DLL, not an executable program. However, the code below, and the ESAPI class libraries, allows this to be run by Eclipse like it is an executable program. 
+    In terms of how the script runs in Eclipse, the program starts here and then starts a GUI, which is a Windows Forms class library. Most of the work of this program is actaully in the GUI.cs file.
+    When the NTCPcalc Visual Studio project is compiled, it will make a single DLL file called "LiverOnlyNTCPcalc.esapi". The .esapi file extensions allows Eclipse to recognize it as an Eclipse script.
+    The only files you need to put in your Eclipse published scripts folder for the Script to work is the DLL file mentioned above, the ESAPI DLL files, and the MathNet.Numerics DLL.
+
+    Copyright (C) 2020 Zackary Thomas Ricci Morelli
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+    I can be contacted at: zackmorelli@gmail.com
+                           zackary.t.morelli@lahey.org
+
+
+*/
+
+namespace VMS.TPS
+{
+    public class Script  // creates a class called Script within the VMS.TPS Namesapce
+    {
+
+       public Script() { }  // instantiates a Script class
+
+
+        // Global Variable Declaration
+
+       public String pl = null;
+
+      // Execution begins with the "Execute" function.
+
+       // Thread Prog = new Thread(Script());
+
+        public void Execute(ScriptContext context)     // PROGRAM START - sending a return to Execute will end the program
+        {
+
+          //  MessageBox.Show("Trig 1");
+            //Variable declaration space
+
+            IEnumerable<PlanSum> Plansums = context.PlanSumsInScope;
+            IEnumerable<PlanSetup> Plans = context.PlansInScope;
+
+            //  MessageBox.Show("Trig 1");
+            if (context.Patient == null)
+            {
+                MessageBox.Show("Please load a patient with a treatment plan before running this script!");
+                return;
+            }
+            
+
+            //GUI starts here
+           //  MessageBox.Show("Trig 4");
+
+            System.Windows.Forms.Application.EnableVisualStyles();
+           // System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);      //This method breaks the script when it runs multiple times, because it will throw an exception if a window has already been created.
+            Start(Plansums, Plans );                                                           //The Windows .NET documentation specifically says this method should NOT be called in a Windows Forms program hosted in another application, like this, so it is ommitted
+                                                                                            //It is a legacy method anyway from eary versions of .NET, there should be no need to call it.
+
+            //Starts GUI for Dose objective check in a separate thread
+            //  Thread GUI = new Thread(new ThreadStart(Go));
+
+            // MessageBox.Show("Trig End");
+
+
+        }
+
+        public static void Start(IEnumerable<PlanSum> Plansums, IEnumerable<PlanSetup> Plans )
+        {
+            System.Windows.Forms.Application.Run(new NTCPcalc.GUI(Plansums, Plans ));
+        }
+
+        //  static void Go (IEnumerable<PlanSum> Plansums, IEnumerable<PlanSetup> Plans, List<string> p1, List<string> p2, List<string> p3, List<string> p4, List<string> p5, List<string> p6, List<string> p7, List<string> p8)
+        //  {
+        //      System.Windows.Forms.Application.Run(new NTCPcalc.GUI(Plansums, Plans, p1, p2, p3, p4, p5, p6, p7, p8));
+        //   }
+                     
+
+
+    }
+}
